aaea20bde844a200cd91c75d7a9d22a058de9460
tom-oroshi <thomas@oroshi.ca>
2022-07-05 07:54:25 -0600

    add dashboard looper example

---
+++ dashboard-looper/dashboard.js
@@ -0,0 +1,11 @@
+// example loop
+// var curURLIndex = -1;
+// const dashboardIframe = document.getElementById("dashboardIframe");
+// setInterval(function () {
+//   ++curURLIndex;
+//   if (curURLIndex >= urlList.length) {
+//     curURLIndex = 0;
+//   }
+//   // Change URL every 20s
+//   dashboardIframe.setAttribute("src", urlList[curURLIndex]);
+// }, 20000);
